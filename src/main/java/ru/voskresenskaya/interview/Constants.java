abada101348409764a5dda61760a62c8ea0adba9
svoskresenskaya <svoskresenskaya@diasoft.ru>
2019-10-13 16:14:03 +0300

    Локализовать выводимые сообщения и вопросы. Annotation-based конфигурация.

--- src/main/java/ru/voskresenskaya/interview/Constants.java
+++ src/main/java/ru/voskresenskaya/interview/Constants.java
@@ -3,8 +3,6 @@ package ru.voskresenskaya.interview;
 public class Constants {
     public static final String ENCODING = "UTF-8";
     public static final int MAX_TRY_COUNT = 3;
-    public static final String TRY_COUNT_DEFAULT_MESSAGE = "- К сожалению, превышено количество попыток для ответа.";
-    public static final String TRY_AGAIN_DEFAULT_MESSAGE = "- Попробуйте еще раз:";
 
     public static final String[] EMPTY_ARGS = new String[] {};
     public static final String GOODBYE_MSG = "¯\\_(ツ)_/¯";

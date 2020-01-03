fd7bd92ad0c74b075b29c179201fb949709e3a45
svoskresenskaya <svoskresenskaya@diasoft.ru>
2020-01-03 22:06:42 +0300

    lesson3- add banner

--- src/main/java/ru/voskresenskaya/interview/Main.java
+++ src/main/java/ru/voskresenskaya/interview/Main.java
@@ -3,24 +3,11 @@ package ru.voskresenskaya.interview;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.*;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import ru.voskresenskaya.interview.service.InterviewService;
-import ru.voskresenskaya.interview.service.ScannerKeeper;
 
 
 @SpringBootApplication
 public class Main {
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource ms
-                = new ReloadableResourceBundleMessageSource();
-        ms.setBasename("/bundle");
-        ms.setDefaultEncoding(Constants.ENCODING);
-        return ms;
-    }
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
